57c39d9fac4d58d6a3f22ef33d9075ef5e58beb8
zhadejiulikai <zhadejiulikai@Demos-MacBook-Pro.local>
2023-08-06 11:46:26 +0800

    first commit

---
+++ Gemfile.php
@@ -0,0 +1,6 @@
+source "https://rubygems.org"
+gem 'github-pages'
+gemspec
+gem 'jekyll-seo-tag'
+gem 'wdm', '>= 0.1.0' if Gem.win_platform?
+gem 'jekyll_image_processing', git: 'https://github.com/benubois/jekyll_image_processing'
